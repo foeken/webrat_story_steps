1395ccd1358ce0fad97419835bc4907c72256bfa
Rob Kaufman <rob@notch8.com>
2008-09-22 19:04:22 -0700

    Polishing up the selenium_helper template to handle not having selenium_specr as a dependancy

--- generators/stories/templates/selenium_helper.rb
+++ generators/stories/templates/selenium_helper.rb
@@ -1,11 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 
-puts "MANUAL: 3 ITEMS REQUIRED"
+puts "INSTRUCTIONS: 3 ITEMS REQUIRED"
 puts "0.) Make sure you have a properly migrated test database and FireFox installed (!)"
 puts "    If you want Safari. Manually configure the proxy to localhost:4444 ... It's wonky under OSX"
-puts "1.) Run a server like this: ./script/server -e test"
-puts "2.) Run: rake selenium:start"
-puts "3.) Then run this file!"
+puts "1.) Run: rake selenium:start"
+puts "2.) Then run this file!"
 
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require File.expand_path(File.dirname(__FILE__) + "/../vendor/plugins/webrat_story_steps/lib/common")
@@ -15,22 +14,25 @@ require "webrat/selenium"
 include WebratStorySteps::Site
 
 # Make a dump of the db before we begin!
-database_name = ActiveRecord::Base.configurations[RAILS_ENV]['database']
-`rake db:dump DATABASE=#{database_name} FILE=selenium_backup.sql`
+task = `rake -D db:dump`
+unless task.gsub(/\(in .*\)\n/, '').empty?
+  database_name = ActiveRecord::Base.configurations[RAILS_ENV]['database']
+  `rake db:dump DATABASE=#{database_name} FILE=selenium_backup.sql`
+end
 
 Dir[File.expand_path("#{File.dirname(__FILE__)}/custom_steps/*.rb")].uniq.each do |file|
   require file
 end
 
 if !@selenium_browser
-  the_browser = SeleniumSpecr::SeleniumRcServer.browser
-  host        = SeleniumSpecr::SeleniumRcServer.host
-  rc_port     = SeleniumSpecr::SeleniumRcServer.port
-  target      = SeleniumSpecr::ApplicationServer.url
+  the_browser = 'firefox' # could be 'chrome' or 'safari'
+  rc_host     = 'localhost'
+  rc_port     = '4444'
+  target      = 'http://localhost:3800'
   timeout     = 10000
 
   @selenium_browser = Selenium::SeleniumDriver.new(
-    host, rc_port, "*#{the_browser}", target, timeout
+    rc_host, rc_port, "*#{the_browser}", target, timeout
   )
 
   @selenium_browser.start
